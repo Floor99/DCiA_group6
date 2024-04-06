197e367151c7eb451fbea412dc5e91ac977ea90f
EvaHaverkort <ealhaverkort@gmail.com>
2024-04-06 07:52:46 +0200

    cleaned up data folder

--- DCiA/dcia/f_load_data.py
+++ DCiA/dcia/f_load_data.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 def load_data():
     # Load people_to_people.csv, grants_to_people.csv and attribute_tabel_final.xlsx
-    knowledge_sharing = pd.read_csv('dcia/static/network_visualisations/people_to_people.csv')
-    grant_to_people_df = pd.read_csv('dcia/static/network_visualisations/grants_to_people.csv')
-    att_table = pd.read_excel('dcia/static/network_visualisations/attributes_final.xlsx')
+    knowledge_sharing = pd.read_csv('dcia/static/data/people_to_people.csv')
+    grant_to_people_df = pd.read_csv('dcia/static/data/grants_to_people.csv')
+    att_table = pd.read_excel('dcia/static/data/attributes_final.xlsx')
 
     # Extract the three attributes: grant awarded, doi year, and organisation
     grant_att_table = att_table[att_table['Grant Awarded'] != 0].iloc[:, 0:3]
